a474e81b97437a84229d8e57d1a75025800bed9d
Rom1 <romain.cernon@mail.com>
2023-04-22 19:49:46 +0200

    Création modules fonctions...

---
+++ L'egende 21/modules/nav.php
@@ -0,0 +1,7 @@
+<!-- navbar -->
+<ul class="topnav">
+    <li><a class="active" href="#home">Home</a></li>
+    <li class="right"><a href="news.php">News</a></li>
+    <li class="right"><a href="#contact">Contact</a></li>
+    <li class="right"><a href="#about">About</a></li>
+</ul>
