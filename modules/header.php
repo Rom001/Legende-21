176279da6c48462da12833a2e7316b6e72f21d21
Rom1 <romain.cernon@mail.com>
2023-04-22 22:49:14 +0200

    Ajout d'un favicon

--- modules/header.php
+++ modules/header.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="author" content="prono">
     <title>Legend 21</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/connexion.css">
 </head>
